68add39ccc9f2d55224b4b3d438c17c18c63797a
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2016-10-22 23:23:00 +0200

    Create toolbox for submission

---
+++ scripts/toolbox.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+""" ML-methods to implement """
+
+import numpy as np
+
+def least_squares_GD(y, tx, gamma, max_iters):
+    """Linear regression using gradient descent"""
+    # init parameters
+    threshold = 1e-8
+    losses = []
+
+    w = np.zeros(tx.shape[1])
+
+    # start the logistic regression
+    for iter in range(max_iters):
+        gradient = calculate_gradient_mse(y, tx, w)
+        
+        # get loss and updated w
+        loss = calculate_loss_mse(y, tx, w)
+        w = w - gamma * gradient
+        
+        # log info
+        if iter % 1000 == 0:
+            print("Current iteration={i}, the loss={l}".format(i=iter, l=loss))
+        # converge criteria
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    print("The loss={l}".format(l=calculate_loss_mse(y, tx, w)))
+    return w
+
+def least_squares_SGD(y, tx, gamma, max_iters):
+    """Linear regression using stochastic gradient descent"""
+    # init parameters
+    threshold = 1e-8
+    losses = []
+
+    w = np.zeros(tx.shape[1])
+
+    # start the logistic regression
+    for iter in range(max_iters):
+        for batch_y, batch_tx in batch_iter(y, tx, 100, num_batches=1):
+            gradient = calculate_gradient_mse(batch_y, batch_tx, w)
+        
+        # get loss and updated w
+        loss = calculate_loss_mse(y, tx, w)
+        w = w - gamma * gradient
+        
+        # log info
+        if iter % 1000 == 0:
+            print("Current iteration={i}, the loss={l}".format(i=iter, l=loss))
+        # converge criteria
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    print("The loss={l}".format(l=calculate_loss_mse(y, tx, w)))
+    return w
+
+def least_squares(y, tx):
+    """calculate the least squares solution."""
+    w_star = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    print("The loss={l}".format(l=calculate_loss_mse(y, tx, w_star)))
+    return w_star
+
+def ridge_regression(y, tx, lambda_):
+    """Ridge regression using normal equations"""
+    M = tx.shape[1]
+    N = y.shape[0]
+    lamb_prime = lambda_ * 2 * N
+    w_star = np.linalg.solve((tx.T @ tx) + lambda_ * np.identity(M), tx.T @ y)
+    print("The loss={l}".format(l=calculate_loss_mse(y, tx, w_star)))
+    return w_star
+
+def logistic_regression(y, tx, gamma, max_iters):
+    """Logistic regression using gradient descent"""
+    # init parameters
+    threshold = 1e-8
+    losses = []
+
+    w = np.zeros(tx.shape[1])
+
+    # start the logistic regression
+    for iter in range(max_iters):
+        gradient = calculate_gradient_log_likelihood(y, tx, w)
+        
+        # get loss and updated w
+        loss = calculate_loss_log_likelihood(y, tx, w)
+        w = w - gamma * gradient
+        
+        # log info
+        if iter % 1000 == 0:
+            print("Current iteration={i}, the loss={l}".format(i=iter, l=loss))
+        # converge criteria
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    print("The loss={l}".format(l=calculate_loss_log_likelihood(y, tx, w)))
+    return w
+
+def reg_logistic_regression(y, tx, lambda_, gamma, max_iters):
+    """Regularized logistic regression using gradient descent"""
+    # init parameters
+    threshold = 1e-8
+    losses = []
+
+    w = np.zeros(tx.shape[1])
+
+    # start the logistic regression
+    for iter in range(max_iters):
+        gradient = calculate_gradient_log_likelihood(y, tx, w) + (lambda_ * 2 * w)
+        
+        # get loss and updated w
+        loss = calculate_loss_log_likelihood(y, tx, w) + lambda_ * np.sum(w**2)
+        w = w - gamma * gradient
+        
+        # log info
+        if iter % 1000 == 0:
+            print("Current iteration={i}, the loss={l}".format(i=iter, l=loss))
+        # converge criteria
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    print("The loss={l}".format(l=calculate_loss_log_likelihood(y, tx, w)))
+    return w
+    
+# LOSS FUNCTIONS
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1/2*np.mean(e**2)
+
+def calculate_loss_mse(y, tx, w):
+    """Calculate the loss using MSE."""
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+def calculate_loss_log_likelihood(y, tx, w):
+    """compute the cost by negative log likelihood."""
+    N = y.shape[0]
+    return np.sum(np.log(1 + np.exp(tx @ w)) - (y.T @ tx @ w))
+
+# linear regression helpers
+def calculate_gradient_mse(y, tx, w):
+    """Compute the gradient of the MSE loss function."""
+    N = y.shape[0]
+    e = y - (tx @ w)
+    return (-1/N) * (tx.T @ e)
+
+# logistic regression helpers
+def sigmoid(t):
+    """apply sigmoid function on t."""
+    return (np.exp(t))/(1 + np.exp(t))
+
+def calculate_gradient_log_likelihood(y, tx, w):
+    """compute the gradient of negative log likelihood."""
+    return tx.T @ (sigmoid(tx @ w) - y)
+
+# stochastic gradient descent helpers
+def batch_iter(y, tx, batch_size, num_batches=None, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+    num_batches_max = int(np.ceil(data_size/batch_size))
+    if num_batches is None:
+        num_batches = num_batches_max
+    else:
+        num_batches = min(num_batches, num_batches_max)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
